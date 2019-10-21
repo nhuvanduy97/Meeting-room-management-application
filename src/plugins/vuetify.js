ce6c618e35f0b3bfbde4ae07d732fd54f61f4a47
Nhu Van Duy (VTI.D2) <duy.nhuvan@vti.vn>
2019-10-21 15:49:05 +0700

    first commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+// import 'vuetify/dist/vuetify.min.css'
+// import '@mdi/font/css/materialdesignicons.css'
+import Vuetify from "vuetify/lib";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: "mdi"
+  }
+});
